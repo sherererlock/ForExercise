1dacddae4e4144806c1897d130cad690f15dcd1e
sherererlock <1225934739@qq.com>
2024-10-24 12:23:32 +0800

    Add PassTemplate

---
+++ Packages/com.unity.render-pipelines.universal/Runtime/PassesCopy/PassTemplate.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+
+namespace UnityEngine.Rendering.Universal.Internal
+{
+    public class PassTemplate : ScriptableRenderPass
+    {
+
+        public PassTemplate()
+        {
+            
+        }
+        
+        private class PassData
+        {
+            
+        }
+        
+    }
+}
